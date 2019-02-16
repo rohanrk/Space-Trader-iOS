05160a52280ff3e73b29707a01ef7ab3f50c48f8
rohanrk <rohanrk2009@gmail.com>
2019-02-16 00:57:04 -0500

    Initial Commit

---
+++ Space-Trader-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Space-Trader-iOS
+//
+//  Created by Rohan Rk on 2/16/19.
+//  Copyright © 2019 Rohan Rk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
